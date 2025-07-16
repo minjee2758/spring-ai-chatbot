626c95f9ad349308a648b962b1a7876f441f00ff
MIN <minjee2758@naver.com>
2025-07-16 09:45:17 +0900

    git iginore 추가

---
+++ src/main/java/com/example/aichatbotspring/AiChatbotSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.aichatbotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiChatbotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiChatbotSpringApplication.class, args);
+    }
+
+}
